6b0b6a1d7314c29186651d559311c025def1cb71
umilton-git <ulysses.github@protonmail.com>
2023-02-10 23:58:08 -0500

    Started work on body

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './Header.js';
+import Home from './Home.js';
 
 function App() {
   return (
     <div className="flex">
       <Header />
+      <Home />
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,16 @@
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-gray-400 hover:text-black m-32 font-bold text-2xl italic w-60 hover:not-italic hover:scale-110 transition-all">
+                "Lorem ipsum dolor sit amet, 
+                consectetur adipiscing elit. 
+                In interdum vitae mi nec suscipit."</h1>
+            <h1 className="text-gray-400 hover:text-black m-32 font-bold text-2xl italic w-60 hover:not-italic hover:scale-110 transition-all absolute right-0">
+                "Lorem ipsum dolor sit amet, 
+                consectetur adipiscing elit. 
+                In interdum vitae mi nec suscipit."</h1>
+        </div>
+    );
+};
+
+export default Home;
