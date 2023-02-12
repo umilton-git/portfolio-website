8e612d8ce359e0048bbb9da808396a510baafb09
umilton-git <ulysses.github@protonmail.com>
2023-02-12 13:40:29 -0500

    Added ThreeJS Cube to homepage

--- src/Footer.js
+++ src/Footer.js
@@ -2,14 +2,13 @@ import Wave from 'react-wavify';
 
 const Footer = () => {
     return (
-        <div className="absolute bottom-0 w-screen">
-            <Wave fill='#000000' paused={false}options={{
-          height: 45,
+        <div className="flex absolute bottom-0 w-screen">
+            <Wave fill='#000000' paused={false} options={{
+          height: 20,
           amplitude: 20,
           speed: 0.30,
-          points: 5
+          bones: 5
         }}/>
-        <p className="bg-black text-white w-30 h-50">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. In interdum vitae mi nec suscipit."</p>
         </div>
     );
 };
--- src/Home.js
+++ src/Home.js
@@ -1,3 +1,36 @@
+import * as THREE from 'three';
+
+const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / 200, 0.01, 10 );
+camera.position.z = 1;
+
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry( 0.5, 0.5, 0.5 );
+const material = new THREE.MeshBasicMaterial({
+    color: 0x000000,
+    wireframe: true
+});
+
+
+const mesh = new THREE.Mesh( geometry, material );
+scene.add( mesh );
+
+const renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } );
+renderer.setSize( window.innerWidth, 200);
+renderer.setAnimationLoop( animation );
+document.body.appendChild( renderer.domElement );
+
+// animation
+
+function animation( time ) {
+
+	mesh.rotation.x = time / 2000;
+	mesh.rotation.y = time / 1000;
+
+	renderer.render( scene, camera );
+
+}
+
 const Home = () => {
     return (
         <div>
@@ -5,6 +38,7 @@ const Home = () => {
                 "Lorem ipsum dolor sit amet, 
                 consectetur adipiscing elit. 
                 In interdum vitae mi nec suscipit."</h1>
+                
             <h1 className="text-gray-400 hover:text-black m-32 font-bold text-2xl italic w-60 hover:not-italic hover:scale-110 transition-all absolute right-0">
                 "Lorem ipsum dolor sit amet, 
                 consectetur adipiscing elit. 
