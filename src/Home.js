db7f18355d34464ca67ebbe1e4e4f9b26a85272f
umilton-git <ulysses.github@protonmail.com>
2023-02-12 14:01:00 -0500

    Small edit on cube

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 
-const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / 200, 0.01, 10 );
+const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / 500, 0.01, 10 );
 camera.position.z = 1;
 
 const scene = new THREE.Scene();
@@ -15,8 +15,8 @@ const material = new THREE.MeshBasicMaterial({
 const mesh = new THREE.Mesh( geometry, material );
 scene.add( mesh );
 
-const renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } );
-renderer.setSize( window.innerWidth, 200);
+const renderer = new THREE.WebGLRenderer( { antialias: false, alpha: true } );
+renderer.setSize( window.innerWidth, 500);
 renderer.setAnimationLoop( animation );
 document.body.appendChild( renderer.domElement );
 
@@ -38,7 +38,7 @@ const Home = () => {
                 "Lorem ipsum dolor sit amet, 
                 consectetur adipiscing elit. 
                 In interdum vitae mi nec suscipit."</h1>
-                
+            <div id="canvas"></div>
             <h1 className="text-gray-400 hover:text-black m-32 font-bold text-2xl italic w-60 hover:not-italic hover:scale-110 transition-all absolute right-0">
                 "Lorem ipsum dolor sit amet, 
                 consectetur adipiscing elit. 
